714feee7ac61d797f2c2bc4e2b80d7c15c9439a9
Shubh Arya <shubh.arya37@gmail.com>
2023-08-09 23:34:54 +0530

    added driver code

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const express =require('express');
 const app = express();
+require("dotenv").config();
+const connectDB = require("./connect");
+const authController = require("./controllers/auth.js");
+const cors = require("cors");
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+try {
+  async function start() {
+    const { Models } = await connectDB();
+    console.log('Connection successful with Database');
+    authController(app, Models);
+    app.listen(3000, console.log("server listening on port:3000"));
+  }
+  start();
+} catch (error) {
+  console.error("Sorry! Cannot start the server!", error);
+}
